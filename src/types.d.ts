36c2fa2d0e89fb36b52d4d241915d30ff9093ff2
Leigh Halliday <leighhalliday@gmail.com>
2020-01-29 11:38:23 -0500

    Fixing type declarations for new puppeteer

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,3 +4,13 @@ interface ParsedReqs {
   website: string;
   image: string;
 }
+
+declare module "puppeteer-core";
+declare module "puppeteer";
+
+type Browser = any;
+type ConnectOptions = any;
+type FetcherOptions = any;
+type BrowserFetcher = any;
+type ChromeArgOptions = any;
+type LaunchOptions = any;
